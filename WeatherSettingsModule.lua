819559da371c72dac462b98677b1d1b75b2daa9f
Johan02 <32437366+Johan02@users.noreply.github.com>
2021-10-16 00:30:33 +0200

    New

--- WeatherSettingsModule.lua
+++ WeatherSettingsModule.lua
@@ -1,5 +1,25 @@
 local Settings = {}
 
+
+Settings.TypesOfWeather = {
+    Sunny = {
+        Chance = 50,
+    },
+
+    Cloudy = {
+        Chance = 50,
+
+    },
+    Rainy = {
+        Chance = 50,
+
+    },
+    Snowy = {
+        Chance = 50,
+    },
+
+}
+
 Settings.CloudSize = {
     Min = 0,
     Max = 10,
@@ -10,13 +30,15 @@ Settings.ChanceOfCloud = {
     MaxWait = 0,
 }
 
+
+
 function Settings.CreateCloud()
     local Cloud = Instance.new('Part')
     local RandomX = math.random(Settings.CloudSize.Min, Settings.CloudSize.Max)
     local RandomZ = math.random(Settings.CloudSize.Min, Settings.CloudSize.Max)
 
     Cloud.Size = Vector3.New(RandomX, 2, RandomZ)
-
+    
     return Cloud
 end
 
